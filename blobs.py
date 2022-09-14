d7c08a888fd245d46cb09432287c46d0c50f75e3
doigiee <doigiee@live.com>
2022-09-14 22:31:17 +1000

    blob art text done

--- blobs.py
+++ blobs.py
@@ -1,11 +1,33 @@
+class XP:
+    def __init__(self, XP):
+        self.value = (XP)
+
 class blob
-    def __init__(self, name, hp, level):
+    available_blobs = [Fire blob, Water blob, Grass blob]
 
-class fire blob
+    def __init__(self, name, species, *, hp = 100, level = 1, atk = 1):
+        self.name = name
+        self.species = species
+        self.hp = hp
+        self.level = level
+        self.atk = atk
+
+class fire_blob
     def super(blob)
 
-class fire blob
+class fire_blob
     def super(blob)
 
-class fire blob
+class fire_blob
     def super(blob)
+
+
+class time_counter 
+    def __init__(self, days_left):
+        days_left = days_left
+        if days_left == 0: game_over
+
+        break
+
+class game_over
+    def __init__(self, ):
